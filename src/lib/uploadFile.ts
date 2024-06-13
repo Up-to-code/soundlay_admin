22a96e63ea03e3f4d742c54575f9f4270def23f2
Ahmed Mansour <133324agh@gmail.com>
2024-06-13 19:59:19 +0300

    ,

--- src/lib/uploadFile.ts
+++ src/lib/uploadFile.ts
@@ -2,7 +2,7 @@
 import { UploadResult, ref, uploadBytes } from "firebase/storage";
 import { storage } from "@/db/DB";
 
-const MAX_SIZE_MB = 2;
+const MAX_SIZE_MB = 10;
 const MAX_DURATION_S = 30;
 
 const validateFile = (file: File): Promise<void> => {
